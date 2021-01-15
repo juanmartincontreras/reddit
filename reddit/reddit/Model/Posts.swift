a6ed736c927388e44830b29260af4556f0d8942c
Juan Martin Contreras <juanmartin@kelsus.com>
2021-01-15 15:24:18 -0300

    refactoring for Post model

--- reddit/reddit/Model/Posts.swift
+++ reddit/reddit/Model/Posts.swift
@@ -10,17 +10,25 @@ import Foundation
 struct Post: Codable {
     let selftext: String
     let clicked: Bool
-    let title, thumbnail, selftextHTML: String
+    let title: String
+    let thumbnail: String
+    let selftextHTML: String
     let visited: Bool
-    let id, author: String
+    let id: String
+    let author: String
     let numComments: Int
     let url: String
     let createdUTC: Double
 
     enum CodingKeys: String, CodingKey {
-        case selftext, clicked, title, thumbnail
+        case selftext
+        case clicked
+        case title
+        case thumbnail
         case selftextHTML = "selftext_html"
-        case visited, id, author
+        case visited
+        case id
+        case author
         case numComments = "num_comments"
         case url
         case createdUTC = "created_utc"
